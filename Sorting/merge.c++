99b919d211e16e88768a1c74fcb6102937c61101
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2025-11-26 00:46:52 +0300

    binarysearch

--- Sorting/merge.c++
+++ Sorting/merge.c++
@@ -1,5 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void merge(int nums[],int n){
+    if n<=1{
+        return nums ;
+    }
+    
+
+}
+
+int main(){
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Before sorting:";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    merge(arr);
+
+    cout<<"After sorting:";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+}
 
 
---
+++ searching/linear.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int binarysearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid; // Target found
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target not found
+}
+int main(){
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Inter the number you want to search: ";
+    int target ;
+    cin >> target; 
+    bool found = false;
+
+}
