7541255796be5169646dfe98631debbeb932e5d3
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-01-01 08:45:45 +0300

    doubly linked list done

--- linkedlist/doublyll.cpp
+++ linkedlist/doublyll.cpp
@@ -72,5 +72,86 @@ void insertvalue3(leli* & head , int value,int pos){
         temp->next->prev = newnode;
     }
     temp->next =newnode;
+}
+// Deleting from the node
+//1.At the beggining 
+void deletevalue1(leli* & head){
+    if(head==NULL){
+        cout<<"list is empty";
+        return;
+    }
+    leli* temp = head;
+    head = head->next;
+    head->prev = NULL;
+    delete temp ;
+}
 
+void deletevalue2(leli*& head){
+    if(head==NULL){
+        cout<<"list is empty";
+        return ;
+    }
+    if(head->next == NULL){
+        delete head;
+        head = NULL;
+    }
+    leli* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->prev->next = NULL;
+    delete temp;
+
+}
+//deleting from any point
+void deletevalue3(leli*& head, int pos){
+    if(head==NULL){
+        cout<<"list is empty";
+        return ;
+    }
+    if(pos == 1){
+        leli* temp = head;
+        head = head->next;
+        delete temp;
+
+    }
+    int count = 1;
+    leli* temp = head ;
+    while(temp->next != NULL && count<pos){
+        temp = temp->next;
+        count++;
+    }
+    temp->next->prev = temp->prev;
+    temp->prev->next = temp->next;
+    delete temp;
+}
+// display doublely linked list 
+void dispalyf(leli*& head ){
+    if(head==NULL){
+        cout<<"LIST IS EMPTY";
+        return;
+    }
+    leli* temp = head;
+    while(temp->next!= NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+void dispalyb(leli* & head){
+    if(head==NULL){
+        cout<<"list is empty";
+    }
+    leli* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    while(temp->prev!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->prev;
+    }
+
+}
+int main(){
+
+    return 0;
 }
