6f9b64732a853eaee9f72141744534e968903648
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-01-01 21:05:57 +0300

    stack and some operation has done

---
+++ stack/arraybased.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+
+int leli[100];
+int top = -1;
+int s = sizeof(leli)/sizeof(leli[0]);
+
+
+void insertvalue(int value ){
+    if(top == s-1){
+        cout<<"stack overflow";
+        return ;
+    }
+    
+    leli[++top] = value;
+}
+
+void deletevalue(){
+    if(top == -1){
+        cout<<"stackunderflow";
+        return ;
+    }
+    cout<<leli[top--];
+    
+}
+void showtop(){
+    if(top == -1){
+        cout<<"underflow";
+        return;
+    }
+    cout<<leli[top];
+}
+void display(){
+    if(top == -1){
+        cout<<"underflow";
+        return;
+    }
+    int temp = top;
+    while (temp  != -1){
+        cout<<leli[temp]<<" ";
+        temp--;
+    }
+}
+
+string reversef(string kena){
+    stack<char> s;
+    for(int i=0 ;i<kena.length();i++){
+        s.push(kena[i]);
+    }
+    
+    string str;
+    for(int i=0;i<kena.length();i++){
+        str += s.top();
+        s.pop();
+    }
+    
+    return str;
+    
+}
+bool checkpalindrome (string kena){
+        stack<char> s;
+    for(int i=0 ;i<kena.length();i++){
+        s.push(kena[i]);
+    }
+    
+    string str;
+    for(int i=0;i<kena.length();i++){
+        str += s.top();
+        s.pop();
+    }
+    
+    return str== kena ;
+}
+int precedence(char op){
+    if(op == '^') return 3;
+    else if(op == '*' || op == '/') return 2;
+    else if (op == '+' || op == '-') return 1;
+    return 0;
+}
+string postfix(string kena){
+    stack<char> s;
+    string str = "";
+    for(int i= 0 ;i<kena.length();i++){
+        if(isalnum(kena[i])){
+            str += kena[i];
+        }
+        else if(kena[i] == "(" ){
+            s.push(kena[i])
+        }
+        else if(kena[i] == ")"){
+            while(!s.empty() && s.top() != "("){
+                str += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else{
+            while(!s.empty() && precedence(s.top)>precedence(kena[i])){
+                str += s.top();
+                s.pop();
+                
+            }
+            s.push(kena[i]);
+        }
+    }
+    while(!s.empty()){
+        str += s.top();
+        s.pop();
+    }
+    return str;
+}
+void tobinary(int kena){
+    stack<int> s;
+    int rem , div ;
+    while(kena > 0){
+        rem = kena % 2;
+        div = kena / 2
+        s.push(rem);
+        kena = div ;
+
+    }
+    while(!s.empty()){
+        cout<<s.top();
+        s.pop();
+    }
+}
+int main(){
+    cout<<reversef("lelisa")<<endl;
+    if (1 == checkpalindrome("lelisa")){
+        cout << true;
+    }
+    else{
+        cout<<false;
+    }
+    return 0;
+}
+
+
